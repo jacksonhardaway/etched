fbc22fe9c4921639ca3f2d76905af915bef22b3c
Jackson H <jackson.hardaway@gmail.com>
2021-06-07 18:14:24 -0700

    :bug: Actually register on Forge

--- forge/src/main/java/me/jaackson/etched/EtchedForge.java
+++ forge/src/main/java/me/jaackson/etched/EtchedForge.java
@@ -1,5 +1,6 @@
 package me.jaackson.etched;
 
+import me.jaackson.etched.bridge.forge.RegistryBridgeImpl;
 import net.minecraftforge.api.distmarker.Dist;
 import net.minecraftforge.eventbus.api.IEventBus;
 import net.minecraftforge.fml.DistExecutor;
@@ -18,9 +19,14 @@ public class EtchedForge {
         IEventBus bus = FMLJavaModLoadingContext.get().getModEventBus();
         bus.addListener(this::commonSetup);
         bus.addListener(this::clientSetup);
-        Etched.commonInit();
 
+        Etched.commonInit();
         DistExecutor.unsafeRunWhenOn(Dist.CLIENT, () -> Etched::clientInit);
+
+        RegistryBridgeImpl.ITEMS.register(bus);
+        RegistryBridgeImpl.BLOCKS.register(bus);
+        RegistryBridgeImpl.BLOCK_ENTITIES.register(bus);
+        RegistryBridgeImpl.SOUND_EVENTS.register(bus);
     }
 
     private void commonSetup(FMLCommonSetupEvent event) {

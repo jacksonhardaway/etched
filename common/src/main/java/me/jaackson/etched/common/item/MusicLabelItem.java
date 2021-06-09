00ea98aa7e3d4ed0d667b7358474ddd91564905d
Jackson H <jackson.hardaway@gmail.com>
2021-06-09 01:16:53 -0700

    :sparkles: Music label is now generated upon crafting

--- common/src/main/java/me/jaackson/etched/common/item/MusicLabelItem.java
+++ common/src/main/java/me/jaackson/etched/common/item/MusicLabelItem.java
@@ -1,9 +1,18 @@
 package me.jaackson.etched.common.item;
 
+import net.minecraft.ChatFormatting;
 import net.minecraft.nbt.CompoundTag;
+import net.minecraft.network.chat.Component;
+import net.minecraft.network.chat.TextComponent;
+import net.minecraft.world.entity.player.Player;
 import net.minecraft.world.item.DyeableLeatherItem;
 import net.minecraft.world.item.Item;
 import net.minecraft.world.item.ItemStack;
+import net.minecraft.world.item.TooltipFlag;
+import net.minecraft.world.level.Level;
+import org.jetbrains.annotations.Nullable;
+
+import java.util.List;
 
 public class MusicLabelItem extends Item implements DyeableLeatherItem {
     public MusicLabelItem(Properties properties) {
@@ -11,8 +20,24 @@ public class MusicLabelItem extends Item implements DyeableLeatherItem {
     }
 
     @Override
+    public void appendHoverText(ItemStack stack, @Nullable Level level, List<Component> list, TooltipFlag flag) {
+        CompoundTag tag = stack.getTagElement("display");
+        if (tag != null) {
+            list.add(new TextComponent(tag.getString("MusicLabel")).withStyle(ChatFormatting.GRAY));
+        }
+    }
+
+    @Override
     public int getColor(ItemStack itemStack) {
         CompoundTag compoundTag = itemStack.getTagElement("display");
         return compoundTag != null && compoundTag.contains("color", 99) ? compoundTag.getInt("color") : 0xFFFFFF;
+    }
+
+    @Override
+    public void onCraftedBy(ItemStack stack, Level level, Player player) {
+        CompoundTag tag = stack.getOrCreateTagElement("display");
+        if (!tag.contains("MusicLabel", 8)) {
+            tag.putString("MusicLabel", player.getScoreboardName() + " - Custom Music");
+        }
     }
 }

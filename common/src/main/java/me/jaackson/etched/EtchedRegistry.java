84be3d9c3d03fe64f515d2a900682096b87791ac
Jackson H <jackson.hardaway@gmail.com>
2021-06-10 02:43:07 -0700

    :fire: Removed etching pattern

--- common/src/main/java/me/jaackson/etched/EtchedRegistry.java
+++ common/src/main/java/me/jaackson/etched/EtchedRegistry.java
@@ -5,14 +5,12 @@ import me.jaackson.etched.bridge.RegistryBridge;
 import me.jaackson.etched.common.block.EtcherBlock;
 import me.jaackson.etched.common.item.BlankMusicDiscItem;
 import me.jaackson.etched.common.item.EtchedMusicDiscItem;
-import me.jaackson.etched.common.item.EtchingPatternItem;
 import me.jaackson.etched.common.item.MusicLabelItem;
 import me.shedaniel.architectury.annotations.ExpectPlatform;
 import net.minecraft.resources.ResourceLocation;
 import net.minecraft.sounds.SoundEvent;
 import net.minecraft.world.item.CreativeModeTab;
 import net.minecraft.world.item.Item;
-import net.minecraft.world.item.Rarity;
 import net.minecraft.world.level.block.Block;
 import net.minecraft.world.level.block.SoundType;
 import net.minecraft.world.level.block.state.BlockBehaviour;
@@ -28,7 +26,6 @@ public class EtchedRegistry {
     public static final Supplier<Item> MUSIC_LABEL = RegistryBridge.registerItem("music_label", new MusicLabelItem(new Item.Properties().stacksTo(1).tab(CreativeModeTab.TAB_MISC)));
     public static final Supplier<Item> BLANK_MUSIC_DISC = RegistryBridge.registerItem("blank_music_disc", new BlankMusicDiscItem(new Item.Properties().stacksTo(1).tab(CreativeModeTab.TAB_MISC)));
     public static final Supplier<Item> ETCHED_MUSIC_DISC = RegistryBridge.registerItem("etched_music_disc", new EtchedMusicDiscItem(new Item.Properties().stacksTo(1)));
-    public static final Supplier<Item> ETCHING_PATTERN = RegistryBridge.registerItem("etching_pattern", new EtchingPatternItem(new Item.Properties().stacksTo(1).tab(CreativeModeTab.TAB_MISC).rarity(Rarity.UNCOMMON)));
 
     public static final Supplier<Block> ETCHER = RegistryBridge.registerBlock("etcher", new EtcherBlock(BlockBehaviour.Properties.of(Material.METAL, MaterialColor.METAL).requiresCorrectToolForDrops().strength(5.0F, 6.0F).sound(SoundType.METAL)), new Item.Properties().tab(CreativeModeTab.TAB_DECORATIONS));
 

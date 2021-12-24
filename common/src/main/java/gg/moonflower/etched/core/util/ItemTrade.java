42d9afae22174a1ebaa1ab7f3645d4473f6140eb
Ocelot <FinntheRaider@gmail.com>
2021-12-24 11:30:53 -0700

    Balanced Bard trades and added trades for note block blocks. Closes #18

--- common/src/main/java/gg/moonflower/etched/core/util/ItemTrade.java
+++ common/src/main/java/gg/moonflower/etched/core/util/ItemTrade.java
@@ -2,10 +2,10 @@ package gg.moonflower.etched.core.util;
 
 import net.minecraft.world.entity.Entity;
 import net.minecraft.world.entity.npc.VillagerTrades;
-import net.minecraft.world.item.Item;
 import net.minecraft.world.item.ItemStack;
 import net.minecraft.world.item.Items;
 import net.minecraft.world.item.trading.MerchantOffer;
+import net.minecraft.world.level.ItemLike;
 
 import java.util.Random;
 import java.util.function.Supplier;
@@ -13,7 +13,7 @@ import java.util.function.Supplier;
 // TODO Make an API in Pollen to make this easier
 public class ItemTrade implements VillagerTrades.ItemListing {
 
-    private final Supplier<Item> item;
+    private final Supplier<? extends ItemLike> item;
     private final int emeralds;
     private final int itemCount;
     private final int maxUses;
@@ -21,15 +21,15 @@ public class ItemTrade implements VillagerTrades.ItemListing {
     private final float priceMultiplier;
     private final boolean sellToVillager;
 
-    public ItemTrade(Supplier<Item> Item, int emeralds, int itemCount, int maxUses, int xpGain) {
+    public ItemTrade(Supplier<? extends ItemLike> Item, int emeralds, int itemCount, int maxUses, int xpGain) {
         this(Item, emeralds, itemCount, maxUses, xpGain, 0.05F, false);
     }
 
-    public ItemTrade(Supplier<Item> Item, int emeralds, int itemCount, int maxUses, int xpGain, boolean sellToVillager) {
+    public ItemTrade(Supplier<? extends ItemLike> Item, int emeralds, int itemCount, int maxUses, int xpGain, boolean sellToVillager) {
         this(Item, emeralds, itemCount, maxUses, xpGain, 0.05F, sellToVillager);
     }
 
-    public ItemTrade(Supplier<Item> Item, int emeralds, int itemCount, int maxUses, int xpGain, float priceMultiplier, boolean sellToVillager) {
+    public ItemTrade(Supplier<? extends ItemLike> Item, int emeralds, int itemCount, int maxUses, int xpGain, float priceMultiplier, boolean sellToVillager) {
         this.item = Item;
         this.emeralds = emeralds;
         this.itemCount = itemCount;
@@ -39,6 +39,7 @@ public class ItemTrade implements VillagerTrades.ItemListing {
         this.sellToVillager = sellToVillager;
     }
 
+    @Override
     public MerchantOffer getOffer(Entity entity, Random random) {
         ItemStack emeralds = new ItemStack(Items.EMERALD, this.emeralds);
         ItemStack item = new ItemStack(this.item.get(), this.itemCount);

ed43401fadba5b0010b5c4a9708da94fa9b6c417
Ocelot <FinntheRaider@gmail.com>
2022-07-20 10:19:11 -0600

    Fixed cache control erroring

--- common/src/main/java/gg/moonflower/etched/api/sound/source/AudioSource.java
+++ common/src/main/java/gg/moonflower/etched/api/sound/source/AudioSource.java
@@ -61,41 +61,46 @@ public interface AudioSource {
             String cacheControl = response.headers().firstValue("Cache-Control").orElse(null);
             if (cacheControl != null) {
                 String[] parts = cacheControl.split(",");
-                for (int i = 0; i < parts.length; i += 2) {
-                    String name = parts[i].trim();
-                    String value = parts[i + 1].trim();
-                    switch (name) {
-                        case "max-age": {
-                            if (cachePriority > 0)
+                for (int i = 0; i < parts.length; i++) {
+                    try {
+                        String[] entry = parts[i].split("=");
+                        String name = entry[0].trim();
+                        String value = entry[1].trim();
+                        switch (name) {
+                            case "max-age": {
+                                if (cachePriority > 0)
+                                    break;
+                                try {
+                                    cacheTime = Integer.parseInt(value);
+                                } catch (NumberFormatException e) {
+                                    LOGGER.error("Invalid max-age: " + value);
+                                }
                                 break;
-                            try {
-                                cacheTime = Integer.parseInt(value);
-                            } catch (NumberFormatException e) {
-                                LOGGER.error("Invalid max-age: " + value);
                             }
-                            break;
-                        }
-                        case "s-maxage": {
-                            cachePriority = 1;
-                            try {
-                                cacheTime = Integer.parseInt(value);
-                            } catch (NumberFormatException e) {
-                                LOGGER.error("Invalid s-maxage: " + value);
+                            case "s-maxage": {
+                                cachePriority = 1;
+                                try {
+                                    cacheTime = Integer.parseInt(value);
+                                } catch (NumberFormatException e) {
+                                    LOGGER.error("Invalid s-maxage: " + value);
+                                }
+                                break;
                             }
-                            break;
+                            // Skip must-revalidate
+                            // Skip no-cache because "hidden" files are already in the temp directory
+                            case "no-store": {
+                                cache = false;
+                                break;
+                            }
+                            // Skip private
+                            // Skip public
+                            // Skip no-transform
+                            // Skip immutable
+                            // Skip stale-while-revalidate
+                            // Skip stale-if-error
                         }
-                        // Skip must-revalidate
-                        // Skip no-cache because "hidden" files are already in the temp directory
-                        case "no-store": {
-                            cache = false;
-                            break;
-                        }
-                        // Skip private
-                        // Skip public
-                        // Skip no-transform
-                        // Skip immutable
-                        // Skip stale-while-revalidate
-                        // Skip stale-if-error
+                    } catch (Exception e) {
+                        LOGGER.error("Invalid response header: {}", parts[i], e);
                     }
                 }
             }

637ca6629a1eb70c8d0ec569afbef58e8514cb69
Jackson <jackson.hardaway@gmail.com>
2022-08-11 08:41:32 -0700

    Updated parrot mixin

--- common/src/main/java/gg/moonflower/etched/core/mixin/client/ParrotMixin.java
+++ common/src/main/java/gg/moonflower/etched/core/mixin/client/ParrotMixin.java
@@ -2,34 +2,60 @@ package gg.moonflower.etched.core.mixin.client;
 
 import gg.moonflower.etched.api.sound.SoundTracker;
 import gg.moonflower.etched.common.item.BoomboxItem;
+import gg.moonflower.etched.core.registry.EtchedTags;
 import net.minecraft.client.Minecraft;
+import net.minecraft.core.BlockPos;
 import net.minecraft.world.entity.Entity;
 import net.minecraft.world.entity.EntityType;
 import net.minecraft.world.entity.LivingEntity;
 import net.minecraft.world.entity.animal.Parrot;
-import net.minecraft.world.entity.animal.ShoulderRidingEntity;
-import net.minecraft.world.entity.player.Player;
 import net.minecraft.world.level.Level;
-import net.minecraft.world.phys.AABB;
+import net.minecraft.world.level.block.Block;
+import net.minecraft.world.level.block.Blocks;
+import net.minecraft.world.level.block.state.BlockState;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.Shadow;
+import org.spongepowered.asm.mixin.Unique;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.Redirect;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
+import org.spongepowered.asm.mixin.injection.callback.LocalCapture;
 
 import java.util.List;
 
 @Mixin(Parrot.class)
-public abstract class ParrotMixin extends ShoulderRidingEntity {
+public abstract class ParrotMixin extends Entity {
+
+    @Shadow private BlockPos jukebox;
 
     @Shadow private boolean partyParrot;
+    @Unique
+    private BlockPos musicPos;
 
-    protected ParrotMixin(EntityType<? extends ShoulderRidingEntity> entityType, Level level) {
+    @Unique
+    private boolean dancing;
+
+    public ParrotMixin(EntityType<?> entityType, Level level) {
         super(entityType, level);
     }
 
+    @Inject(method = "aiStep", at = @At("HEAD"), locals = LocalCapture.CAPTURE_FAILEXCEPTION)
+    public void capture(CallbackInfo ci) {
+        this.musicPos = this.jukebox;
+        this.dancing = this.partyParrot;
+    }
+
     @Inject(method = "aiStep", at = @At(value = "INVOKE", target = "Lnet/minecraft/world/entity/animal/ShoulderRidingEntity;aiStep()V"))
-    public void checkEntitySound(CallbackInfo ci) {
+    public void addAudioProviders(CallbackInfo ci) {
+        if (this.musicPos == null || !this.musicPos.closerThan(this.position(), 3.46) || !this.level.getBlockState(this.musicPos).is(Blocks.JUKEBOX) && !this.level.getBlockState(this.musicPos).is(EtchedTags.AUDIO_PROVIDER)) {
+            this.partyParrot = false;
+            this.jukebox = null;
+        } else {
+            this.partyParrot = this.dancing;
+            this.jukebox = this.musicPos;
+        }
+
         if (this.level.isClientSide()) {
             List<Entity> entities = this.level.getEntities(this, this.getBoundingBox().inflate(3.45), entity -> {
                 if (!entity.isAlive() || entity.isSpectator())
@@ -39,6 +65,7 @@ public abstract class ParrotMixin extends ShoulderRidingEntity {
 
                 return SoundTracker.getEntitySound(entity.getId()) != null;
             });
+
             if (!entities.isEmpty())
                 this.partyParrot = true;
         }

ca5491efc8b6785495a485ead5307d06d80ddad4
Ocelot <FinntheRaider@gmail.com>
2023-07-21 13:44:15 -0600

    Fixed mixin crash. Fixes #49

--- common/src/main/java/gg/moonflower/etched/core/mixin/client/ItemStackMixin.java
+++ common/src/main/java/gg/moonflower/etched/core/mixin/client/ItemStackMixin.java
@@ -1,10 +1,7 @@
 package gg.moonflower.etched.core.mixin.client;
 
 import gg.moonflower.etched.common.item.BoomboxItem;
-import gg.moonflower.etched.core.Etched;
-import net.minecraft.ChatFormatting;
 import net.minecraft.network.chat.Component;
-import net.minecraft.network.chat.TranslatableComponent;
 import net.minecraft.world.entity.player.Player;
 import net.minecraft.world.item.Item;
 import net.minecraft.world.item.ItemStack;
@@ -14,7 +11,6 @@ import org.spongepowered.asm.mixin.Shadow;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
-import org.spongepowered.asm.mixin.injection.callback.LocalCapture;
 
 import java.util.List;
 
@@ -24,9 +20,10 @@ public abstract class ItemStackMixin {
     @Shadow
     public abstract Item getItem();
 
-    @Inject(method = "getTooltipLines", at = @At("TAIL"), locals = LocalCapture.CAPTURE_FAILEXCEPTION)
-    public void addBoomboxStatus(Player player, TooltipFlag isAdvanced, CallbackInfoReturnable<List<Component>> cir, List<Component> lines) {
-        if (this.getItem() instanceof BoomboxItem && BoomboxItem.isPaused((ItemStack) (Object) this))
-            lines.add(BoomboxItem.PAUSED);
+    @Inject(method = "getTooltipLines", at = @At("TAIL"))
+    public void addBoomboxStatus(Player player, TooltipFlag isAdvanced, CallbackInfoReturnable<List<Component>> cir) {
+        if (this.getItem() instanceof BoomboxItem && BoomboxItem.isPaused((ItemStack) (Object) this)) {
+            cir.getReturnValue().add(BoomboxItem.PAUSED);
+        }
     }
 }
